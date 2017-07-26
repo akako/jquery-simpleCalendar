f42e3e3b8c42369229bcdbe94987da6e2562f171
賀好昭仁 <kako@qnote.co.jp>
2017-07-26 14:38:42 +0900

    [modify]Options value can change in nextClick and previousClick.

--- jquery.simpleCalendar.js
+++ jquery.simpleCalendar.js
@@ -1,8 +1,9 @@
 /**
- * @author Kako Akihito <akakopublic@gmail.com>
- * @license New BSD License
- * @version 1.0.0
+ * @author Akihito Kako <akakopublic@gmail.com>
+ * @license MIT License
+ * @version 1.0.1
  */
+"use strict";
 (function($) {
     var getItems = function(year, month) {
         var dates = [];
@@ -14,7 +15,7 @@
         for (var i = start + 1; date <= end; i++) {
             dates.push({
                 date: date,
-                active: currentMonthStart <= date && date <= currentMonthEnd ? true : false
+                active: currentMonthStart <= date && date <= currentMonthEnd
             });
             date = new Date(year, month - 1, i);
         }
@@ -38,11 +39,11 @@
             previousMonthFormatter: function(date) {return '< ' + ('0' + (date.getMonth() + 1)).slice(-2);},
             nextMonthFormatter: function(date) {return ('0' + (date.getMonth() + 1)).slice(-2) + ' >';}
         };
-        if (typeof(options) != 'object') {
+        if (typeof(options) !== 'object') {
             options = defaultOptions;
         } else {
             for (var key in defaultOptions) {
-                if (typeof(options[key]) == 'undefined') {
+                if (undefined === options[key]) {
                     options[key] = defaultOptions[key];
                 }
             }
@@ -51,30 +52,40 @@
         options.year = formatterDate.getFullYear();
         options.month = formatterDate.getMonth() + 1;
 
-        parent.html('');
+        parent.empty();
         var table = $('<table />').addClass(options.tableClass).appendTo($(this));
         var controllerRow = $('<tr />').appendTo(table);
         var controllerTd = $('<td />').attr('colspan', 7).appendTo(controllerRow);
         var controllerCell = $('<div />').addClass('controller').appendTo(controllerTd);
-        if (typeof(options.previousMonthLimit) == 'undefined' || options.previousMonthLimit > 0) {
-            var prevMonth = (options.month - 1) % 12 == 0 ? 12 : (options.month - 1) % 12;
+        if (options.previousMonthLimit === undefined || options.previousMonthLimit > 0) {
             $('<div />').addClass('prev').append(options.previousMonthFormatter(new Date(options.year, options.month - 2, 1))).bind('click', function(){
                 options.month--;
-                if (typeof(options.previousMonthLimit) != 'undefined') options.previousMonthLimit--;
-                if (typeof(options.nextMonthLimit) != 'undefined') options.nextMonthLimit++;
-                if (typeof(options.previousClick) == 'function') 
-                    options.previousClick(options);
+                options.year += Math.floor((options.month + 12) / 12) - 1;
+                options.month = options.month % 12;
+                if (options.previousMonthLimit !== undefined) options.previousMonthLimit--;
+                if (options.nextMonthLimit !== undefined) options.nextMonthLimit++;
+                if (typeof(options.previousClick) === 'function') {
+                    var newOptions = options.previousClick(options);
+                    if (undefined !== newOptions) {
+                        options = newOptions;
+                    }
+                }
                 parent.simpleCalendar(options);
             }).appendTo(controllerCell);
         }
-        if (typeof(options.nextMonthLimit) == 'undefined' || options.nextMonthLimit > 0) {
-            var nextMonth = (options.month + 1) % 12 == 0 ? 12 : (options.month + 1) % 12;
+        if (options.nextMonthLimit === undefined || options.nextMonthLimit > 0) {
             $('<div />').addClass('next').append(options.nextMonthFormatter(new Date(options.year, options.month, 1))).bind('click', function() {
                 options.month++;
-                if (typeof(options.previousMonthLimit) != 'undefined') options.previousMonthLimit++;
-                if (typeof(options.nextMonthLimit) != 'undefined') options.nextMonthLimit--;
-                if (typeof(options.nextClick) == 'function') 
-                    options.nextClick(options);
+                options.year += Math.floor((options.month + 12) / 12) - 1;
+                options.month = options.month % 12;
+                if (options.previousMonthLimit !== undefined) options.previousMonthLimit++;
+                if (options.nextMonthLimit !== undefined) options.nextMonthLimit--;
+                if (typeof(options.nextClick) === 'function') {
+                    var newOptions = options.nextClick(options);
+                    if (undefined !== newOptions) {
+                        options = newOptions;
+                    }
+                }
                 parent.simpleCalendar(options);
             }).appendTo(controllerCell);
         }
@@ -87,20 +98,20 @@
         var items = getItems(options.year, options.month);
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
-            if (item.date.getDay() == 0) {
+            if (item.date.getDay() === 0) {
                 var row = $('<tr />').appendTo(table);
             }
             var cell = $('<td />').append(item.date.getDate()).addClass(item.active ? 'active' : 'inactive').appendTo(row);
             if (item.active) {
-                if (item.date.getDay() == 0) {
+                if (item.date.getDay() === 0) {
                     cell.addClass('sunday');
-                } else if (item.date.getDay() == 6) {
+                } else if (item.date.getDay() === 6) {
                     cell.addClass('saturday');
                 }
-                if (item.date.getFullYear() == today.getFullYear() && item.date.getMonth() == today.getMonth() && item.date.getDate() == today.getDate()) {
+                if (item.date.getFullYear() === today.getFullYear() && item.date.getMonth() === today.getMonth() && item.date.getDate() === today.getDate()) {
                     cell.addClass('today');
                 }
-                if (typeof(options.cellClick) == 'function') {
+                if (typeof(options.cellClick) === 'function') {
                     cell.bind('click', {
                         date: item.date
                     }, options.cellClick);
@@ -108,7 +119,7 @@
                 for (var className in options.cellClass) {
                     var classTargetDates = options.cellClass[className];
                     for (var j = 0; j < classTargetDates.length; j++) {
-                        if (classTargetDates[j].toString() == item.date.toString()) {
+                        if (classTargetDates[j].toString() === item.date.toString()) {
                             cell.addClass(className);
                         }
                     }
